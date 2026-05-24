ceadabea318a4448dda91cd322bfa1b78dbc890f
berkal749 <berkalachrefpro@gmail.com>
2026-05-24 02:21:38 +0100

    Add Gradle configuration files for Android backup project
    - Created gradle.properties to configure JVM arguments and AndroidX support.
    - Added gradle-wrapper.properties to specify Gradle distribution settings.
    - Introduced settings.gradle.kts to manage plugin dependencies and include Flutter SDK.

--- android_backup/app/build.gradle.kts
+++ android_backup/app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+    id("org.jetbrains.kotlin.android") 
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -13,11 +14,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -29,7 +25,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        multiDexEnabled = true
     }
 
     buildTypes {
@@ -41,11 +36,18 @@ android {
     }
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
